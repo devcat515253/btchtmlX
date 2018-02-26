a3de42cd4133e74400872ebc0722b049c72e68ab
h1tan <axe.dota@mail.ru>
2018-02-26 18:56:00 +0200

    adptive1

--- app/js/common.js
+++ app/js/common.js
@@ -4,10 +4,16 @@ $(function() {
 		e.preventDefault();
 
 		var _this = $(this);
-
 		_this.closest(".dropdown").toggleClass("_opened");
-
 	});
+
+	$(".header-menu .menu>li .dropdown-link").click(function (e) {
+		e.preventDefault();
+
+		var _this = $(this);
+		_this.closest(".transform-to-mobile").toggleClass("sub-opened");
+	});
+
 
 
 	$(document).click( function(event){
@@ -27,8 +33,14 @@ $(function() {
 	$(".hamburger").click(function(e) {
 		e.preventDefault();
 
+
+		if ( $("body").hasClass("menu-mobile__opened")) {
+			$(".transform-to-mobile").removeClass("sub-opened");
+		}
+
 		$(this).toggleClass("is-active");
 		$("body").toggleClass("menu-mobile__opened");
+		
 	});
 
 
