62d20d93d4a558854025516a9ab093c9dd25d088
h1tan <axe.dota@mail.ru>
2018-02-26 14:34:29 +0200

    Start

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,29 @@
 $(function() {
 
-	// Custom JS
+	$(".js-dropdown-link").click(function (e) {
+		e.preventDefault();
+
+		var _this = $(this);
+
+		_this.closest(".dropdown").toggleClass("_opened");
+
+	});
+
+
+	$(document).click( function(event){
+		if( $(event.target).closest(".dropdown").length ) // родитель выпадающего списка
+		return;
+		$(".dropdown").removeClass("_opened");
+		event.stopPropagation();
+	});
+
+	$(".rippler").rippler({
+		effectClass     :  'rippler-effect',
+	    effectSize      :  0,      // Default size (width & height)
+	    addElement      :  'div',   // e.g. 'svg'(feature)
+	    duration        :  400
+	});
+
+
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,7 @@ gulp.task('common-js', function() {
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/rippler-master/dist/js/jquery.rippler.min.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
